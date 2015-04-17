16f30d19c64f25fc3b613373b45070dc6da91d6b
Rasmus <rasmus.webs@gmail.com>
2015-04-17 15:04:50 +0200

    Items + Weapons_1.0

--- src/main/java/edu/chl/Game/object/Item.java
+++ src/main/java/edu/chl/Game/object/Item.java
@@ -11,10 +11,18 @@ public abstract class Item extends GameObject{
 	private String info;
 	private BufferedImage buffImage;
 	
+	//Temporary
+	private int x,y, width,height; 
+	
 	Item(int x, int y, int width, int height, Id id, GameHandler handeler, String info, BufferedImage buffImage){
 		super(/*x,y,width,height,id,handeler*/);
-		this.info = info;
+		this.x = x;
+		this.y = y;
 		this.buffImage = buffImage;
+		this.width = width;
+		this.height = height;
+		this.info = info;
+		
 		/* suggestions we can have two constructors one if there is an 
 		 * bufferd image and another if there is an grafical-object 
 		 */
@@ -31,5 +39,34 @@ public abstract class Item extends GameObject{
 	public String getInfo(){
 		return info;
 	}
+	
+	public BufferedImage getBuffImage(){
+		return buffImage;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	protected void setX(int x) {
+		this.x = x;
+	}
+
+	protected void setY(int y) {
+		this.y = y;
+	}
+	
 	
 }
--- src/main/java/edu/chl/Game/object/WeaponA.java
+++ src/main/java/edu/chl/Game/object/WeaponA.java
@@ -1,5 +1,22 @@
 package edu.chl.Game.object;
 
-public class WeaponA {
+import java.awt.image.BufferedImage;
+
+import edu.chl.Game.handler.GameHandler;
+
+public class WeaponA extends Weapon{
+
+	WeaponA(int x, int y, int width, int height, Id id, GameHandler handeler,
+			String info, BufferedImage buffImage) {
+		super(x, y, width, height, id, handeler, info, buffImage);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void effect() {
+		//
+		
+	}
 
 }
