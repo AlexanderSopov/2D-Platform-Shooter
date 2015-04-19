140649179528849eff6127199660c38520700a67
oliver <olivert@student.chalmers.se>
2015-04-19 22:20:21 +0200

    Tile class
    unfinished Tile class

--- src/main/java/edu/chl/Game/tile/Tile.java
+++ src/main/java/edu/chl/Game/tile/Tile.java
@@ -1,10 +1,17 @@
 package edu.chl.Game.tile;
 
 import edu.chl.Game.object.GameObject;
+import edu.chl.Game.object.Id;
 
 public abstract class Tile extends GameObject{
+
+	public Tile(int x, int y, int width, int height, int vX, int vY,
+			boolean isSolid, Id id) {
+		super(x, y, width, height, vX, vY, isSolid, id);
+		// TODO Auto-generated constructor stub
+	}
 	
-	private String name; // test kod
+
 	
 	
 
