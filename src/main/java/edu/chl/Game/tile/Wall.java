67415b86a20e3cf6d94bd04ad3c94c7af4e7ce9d
oliver <olivert@student.chalmers.se>
2015-04-19 22:19:22 +0200

    GameObject implemented
    I implemented the GameObject class

--- src/main/java/edu/chl/Game/tile/Wall.java
+++ src/main/java/edu/chl/Game/tile/Wall.java
@@ -1,5 +1,27 @@
 package edu.chl.Game.tile;
 
+import java.awt.Graphics;
+
+import edu.chl.Game.object.Id;
+
 public class Wall extends Tile{
+
+	public Wall(int x, int y, int width, int height, int vX, int vY,
+			boolean isSolid, Id id) {
+		super(x, y, width, height, vX, vY, isSolid, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
