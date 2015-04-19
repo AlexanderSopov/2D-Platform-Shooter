e57a5ac47091a7517744b52c8edc4b043f8bee23
Mansoor <mansoorbeck@gmail.com>
2015-04-20 01:45:28 +0200

    La till lite saker.

--- src/main/java/edu/chl/Game/view/Frame.java
+++ src/main/java/edu/chl/Game/view/Frame.java
@@ -1,11 +1,65 @@
 package edu.chl.Game.view;
 
 
+import java.awt.Dimension;
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+
 /**
+ * Frame is responsible for creating the frame.
  * 
- * @author Mansoor
+ * @author Mansoor Beck
  * @version 1.0
  */
-public class Frame {
 
+public class Frame extends Canvas {
+
+	
+	/**
+	 * Auto generated serialVersion.
+	 */
+	private static final long serialVersionUID = 2301826460122833631L;
+	
+	/**
+	 * The standard width to begin with.
+	 */
+	private static final int width = 270;
+	
+	/**
+	 * The standard height to begin with.
+	 */
+	private static final int height = width/14*10;
+	
+	/**
+	 * Scaler to be used to stretch an object
+	 * with length of 4.
+	 */
+	private static final int scale = 4;
+	
+	public Frame() {
+		
+		Dimension dimSize = new Dimension(width*scale, height*scale);
+		setPreferredSize(dimSize);
+		setMinimumSize(dimSize);
+		setMaximumSize(dimSize);
+	}
+	
+	private void gameFrame() {
+		
+		JFrame frame = new JFrame("Mario Battlefield");
+		frame.add(new Frame());
+		frame.setVisible(true);
+		frame.pack();
+		
+		
+		/**
+		 * Resizable from dragging the window is false. 
+		 * Resizable should be in the game in resolution.
+		 */
+		frame.setResizable(true);
+		
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
--- src/main/java/edu/chl/Game/view/GameThread.java
+++ src/main/java/edu/chl/Game/view/GameThread.java
@@ -1,5 +1,25 @@
 package edu.chl.Game.view;
 
-public class GameThread {
 
+
+
+/**
+ * GameThread is responsible for creating the underlying thread.
+ * To make sure the game is running parallel methods beneath the hood.
+ * 
+ * @author Mansoor Beck
+ * @version 1.0
+ */
+public class GameThread implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		GameThread startGame = new GameThread();
+	}
 }
