d354a52a38e075a85843929db002748ab91666ac
Mansoor <mansoorbeck@gmail.com>
2015-04-20 01:58:37 +0200

    La till saker. Main focus är att lära mig git

--- src/main/java/edu/chl/Game/view/GameThread.java
+++ src/main/java/edu/chl/Game/view/GameThread.java
@@ -12,14 +12,37 @@ package edu.chl.Game.view;
  */
 public class GameThread implements Runnable {
 
+	private boolean isRunning = true;
+	
+	public GameThread() {
+		
+		
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
 	
+	/**
+	 * Check whetever the game is running or not.
+	 * @return isRunning - True if game is running, false otherwise.
+	 */
+	public boolean isGameRunning() {
+		return this.isRunning;
+	}
+	
+	/**
+	 * 
+	 * @param b 
+	 */
+	public void setGameRunning(boolean b) {
+		this.isRunning = b;
+	}
 	public static void main(String[] args) {
 		
 		GameThread startGame = new GameThread();
 	}
+	
+	
 }
