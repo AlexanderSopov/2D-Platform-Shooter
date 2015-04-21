b04a5f40decef475ad30c73ccc4413cc8e83b49a
Mansoor <mansoorbeck@gmail.com>
2015-04-21 20:52:33 +0200

    La till Update metoden från Observer samt rensade upp och kommenterade meoderna. Alex och Rasmus titta på klassen.

--- src/main/java/edu/chl/Game/view/GameThread.java
+++ src/main/java/edu/chl/Game/view/GameThread.java
@@ -3,10 +3,17 @@ package edu.chl.Game.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.util.Observable;
+import java.util.Observer;
 
 import edu.chl.Game.handler.GameHandler;
 
-public class GameThread implements Runnable {
+/**
+ * 
+ * @author Mansoor
+ * @version 1.0
+ */
+public class GameThread implements Runnable, Observer {
 	
 	private Thread thread;
 	private Frame frame;
@@ -22,18 +29,28 @@ public class GameThread implements Runnable {
 		thread = new Thread(this);
 		frame = new Frame();
 		handler = new GameHandler(thread, frame);
-	
 	}
 	
+	/**
+	 * If Game is running return true otherwise false.
+	 * @return running 
+	 */
+	private boolean isRunning() {
+		return this.running;
+	}
+	
+	/**
+	 * 
+	 */
 	public synchronized void start(){
-		if(!running){
+		if(isRunning()) {
 			running = true;
 			thread.start();
 		}
 	}
 	
 	public synchronized void interrupt(){
-		if(running){
+		if(isRunning()){
 			running = false;
 			try {
 				thread.join();
@@ -49,25 +66,46 @@ public class GameThread implements Runnable {
 		timer();	
 	}
 	
+	/**
+	 * Update Timer, create a Buffer and update the handler.
+	 */
 	public void update(){
 		handler.update();
-		render();
+		createBuffer();
 		printTimer();
 	}
 
-	public void render(){
+	/**
+	 * Create a Buffer with maximum number of 3 and start rendering.
+	 */
+	public void createBuffer(){
 		BufferStrategy bs = frame.getBufferStrategy();
 		if(bs == null){
 			frame.createBufferStrategy(3);
 			return;
 		}
-		Graphics g = bs.getDrawGraphics();
+		renderGraphics(bs);
+	}
+	
+	/**
+	 * Rendering the Graphics to the screen.
+	 * @param b - BufferStrategy to access the methods in BufferStrategy class.
+	 */
+	public void renderGraphics(BufferStrategy b) {
+		Graphics g = b.getDrawGraphics();
 		g.setColor(new Color(135, 206, 235));
 		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
 		g.translate(handler.getCamera().getX(), handler.getCamera().getY());
 		handler.render(g);
 		g.dispose();
-		bs.show();
+		showGraphics(b);
+	}
+	
+	/**
+	 * Show the Graphics.
+	 */
+	public void showGraphics(BufferStrategy b) {
+		b.show();
 	}
 	
 	private void timer(){
@@ -84,6 +122,10 @@ public class GameThread implements Runnable {
 		interrupt();	
 	}
 	
+	/**
+	 * 
+	 * @param d
+	 */
 	private void timeToUpdate(double d){
 		if(d>1){
 			delta=delta-1;
@@ -91,14 +133,33 @@ public class GameThread implements Runnable {
 		}
 	}
 	
-
-
+	/**
+	 * Printing frames per second.
+	 */
 	private void printTimer(){
 		Frame++;
-		if(Frame==60){
+		if(isFrame()){
 			System.out.println(Frame + " updates/frames in the " + second + "th second");
 			second++;
 			Frame=1;
+		}
+	}
+	
+	/**
+	 * Checking if Frame is equal to 60.
+	 * @return Frame - If frame is not 60 then return false otherwise true.
+	 */
+	private boolean isFrame() {
+		return Frame==60;
+	}
+	
+	/**
+	 * If observer have been change then update.
+	 * @see java.util.Observer;
+	 */
+	public void update(Observable o, Object arg) {
+		if(o.hasChanged()) {
+			update();
 		}
 	}
 
