d4bc66b3326066e510a31358463cc9bbd237759b
Rasmus <rasmus.webs@gmail.com>
2015-04-24 12:03:22 +0200

    merged GameThread

--- src/main/java/edu/chl/Game/view/GameThread.java
+++ src/main/java/edu/chl/Game/view/GameThread.java
@@ -3,29 +3,44 @@ package edu.chl.Game.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.util.Observable;
 
 import edu.chl.Game.handler.GameHandler;
 
-public class GameThread implements Runnable {
+/**
+ * 
+ * @author Mansoor, Alexander
+ * @version 1.0
+ */
+public class GameThread extends Observable implements Runnable {
+	
+	
 	
 	private Thread thread;
 	private Frame frame;
-	private GameHandler handler;
+	public GameHandler handler;
 	
 	private boolean running = false;
+	
+	private double delta = 0.0;
+	private int Frame=1;
+	private int second=1;
 	
 	public GameThread(){
 		thread = new Thread(this);
 		frame = new Frame();
 		handler = new GameHandler(thread, frame);
-	
 	}
-	
+
+	/**
+	 * 
+	 */
 	public synchronized void start(){
-		if(!running){
-			running = true;
-			thread.start();
-		}
+		if(running)
+			return;
+		running = true;
+		thread.start();
+		
 	}
 	
 	public synchronized void interrupt(){
@@ -41,53 +56,89 @@ public class GameThread implements Runnable {
 	
 	@Override
 	public void run() {
-		//init();
 		frame.requestFocus();
-		long previousTime = System.nanoTime();
-		long timer = System.currentTimeMillis();
-		double delta = 0.0;
-		double ns = 1000000000.0/60.0;
-		int frames = 0;
-		int updates = 0;
-		while(running){
-			long currentTime = System.nanoTime();
-			delta+=(currentTime - previousTime)/ns;
-			previousTime = currentTime;
-			while(delta>=1){
-				handler.update();
-				updates++;
-				delta--;
-			}
-			render();
-			frames++;
-			if(System.currentTimeMillis()-timer > 1000){
-				timer += 1000;
-				System.out.println(frames + " FPS, " + updates + " refreshrate");
-				updates = 0;
-				frames = 0;
-			}
-		}
-		interrupt();	
+		timer();	
 	}
 	
+	/**
+	 * Update Timer, create a Buffer and update the handler.
+	 */
+	public void update(){
+		setChanged();
+		render();
+		printTimer();
+	}
+
+	/**
+	 * Create a Buffer with maximum number of 3 and start rendering.
+	 */
 	public void render(){
 		BufferStrategy bs = frame.getBufferStrategy();
 		if(bs == null){
 			frame.createBufferStrategy(3);
 			return;
 		}
-		Graphics g = bs.getDrawGraphics();
+		renderGraphics(bs);
+	}
+	
+	/**
+	 * Rendering the Graphics to the screen.
+	 * @param b - BufferStrategy to access the methods in BufferStrategy class.
+	 */
+	public void renderGraphics(BufferStrategy b) {
+		Graphics g = b.getDrawGraphics();
 		g.setColor(new Color(135, 206, 235));
 		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
 		g.translate(handler.getCamera().getX(), handler.getCamera().getY());
+		notifyObservers((Object)g);
 		handler.render(g);
 		g.dispose();
-		bs.show();
+		b.show();
 	}
 	
-	
-	public void update(){
-		handler.update();
-	}
 
+	private void timer(){
+		long timeSnap1 = System.nanoTime();
+		double nanosec = 1000000000.0;
+		long timeSnap2 = System.nanoTime();
+		delta = (timeSnap2 - timeSnap1)*60/nanosec;
+		while(running){
+			timeSnap1 = timeSnap2;
+			timeSnap2 = System.nanoTime();
+			delta += (timeSnap2 - timeSnap1 )*60/nanosec;
+			timeToUpdate(delta);
+		}
+		interrupt();	
+	}
+	
+	/**
+	 * 
+	 * @param d
+	 */
+	private void timeToUpdate(double d){
+		if(d>1){
+			delta=delta-1;
+			update();
+		}
+	}
+	
+	/**
+	 * Printing frames per second.
+	 */
+	private void printTimer(){
+		Frame++;
+		if(isFrame()){
+			System.out.println(Frame + " updates/frames in the " + second + "th second");
+			second++;
+			Frame=1;
+		}
+	}
+	
+	/**
+	 * Checking if Frame is equal to 60.
+	 * @return Frame - If frame is not 60 then return false otherwise true.
+	 */
+	private boolean isFrame() {
+		return Frame==60;
+	}
 }
