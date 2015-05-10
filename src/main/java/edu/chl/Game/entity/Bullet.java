af699e7327539f25751883b28e15ea9b5a2122ef
Rasmus <rasmus.webs@gmail.com>
2015-05-10 16:33:04 +0200

    bulletY

--- src/main/java/edu/chl/Game/entity/Bullet.java
+++ src/main/java/edu/chl/Game/entity/Bullet.java
@@ -6,6 +6,7 @@ import edu.chl.Game.view.Frame;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 
 /**
  *
@@ -16,13 +17,15 @@ public class Bullet extends Entity{
     private int targetPosX,targetPosY, speed;
     private float xVelocity, yVelocity;
     private int centerX, centerY;
-    double angle;
+    private double angle;
+    private AffineTransform at;
     
 
     public Bullet(int x, int y, int width, int height, boolean solid, Id id,
 			GameHandler handler,int targetPosX, int targetPosY, int speed, double angle) {
         super(x, y, width, height, solid, id, handler);
         
+                at = new AffineTransform();
                 
                 this.angle = angle;
                 
@@ -30,7 +33,7 @@ public class Bullet extends Entity{
                 
 		this.setTargetPosY(targetPosY);
 		this.speed = speed;
-                
+                this.speed = 1;
                 this.centerX = getX() ;
                 this.centerY = getY() ;
                
@@ -39,8 +42,13 @@ public class Bullet extends Entity{
     @Override
     public void render(Graphics g) {
         g.setColor(Color.BLACK);
+       // at = at.getRotateInstance(angle, centerX, centerY);
+        
         ((Graphics2D)g).rotate(angle, centerX, centerY);
-	g.fillOval(getX()-(getWidth()/2)+50,getY()-(getHeight()/2), getWidth(), getHeight());
+	g.fillOval(getX()-(getWidth()/2)+50,this.centerY-(getHeight()/2), getWidth(), getHeight());
+        g.setColor(Color.red);
+        g.drawString(this.getY()+"", this.getX(), this.centerY-(getHeight()/2));
+        
         ((Graphics2D)g).rotate(-angle, centerX, centerY);
 
 	
@@ -49,9 +57,12 @@ public class Bullet extends Entity{
     @Override
     public void update() {
         
+       
+        
         this.xVelocity = speed;
 	setX(getX() + (int)xVelocity);
-	
+	//setX((int)((getX() - this.centerX) * Math.cos(this.angle) - (getY() - this.centerY) * Math.sin(this.angle) + this.centerX));
+        setY((int)((getY() - this.centerY) * Math.cos(this.angle) + (getX() - this.centerX) * Math.sin(this.angle) + this.centerY));
     }
     
     	public int getTargetPosX() {
