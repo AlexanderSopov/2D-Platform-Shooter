ee72a7ede724952cc39afd4e770c2284c2579dd2
Mansoor <mansoorbeck@gmail.com>
2015-05-11 17:29:06 +0200

    Kommenterat och lagt till en SFX klass for det behovs ljud OCH musik till spelet

--- src/main/java/edu/chl/Game/sound/Music.java
+++ src/main/java/edu/chl/Game/sound/Music.java
@@ -1,9 +1,16 @@
 package edu.chl.Game.sound;
 
+import java.util.LinkedList;
+
 import javax.sound.sampled.*;
 
+
+/**
+ * Music for the Game.
+ */
 public class Music {
 	
+	private LinkedList<Music> musicList = new LinkedList<>();
 	private Clip clip;
 	
 	public Music(String s) {
@@ -33,17 +40,48 @@ public class Music {
 		}
 	}
 	
+	/**
+	 * Get the Music List.
+	 * @return - Returns the Music List of type LinkedList<Music>
+	 */
+	public LinkedList<Music> getMusicList() {
+		return this.musicList;
+	}
+	/**
+	 * Add music to the music list
+	 * @param music - Add music of the type Music.
+	 */
+	public void addMusicList(Music music) {
+		musicList.add(music);
+	}
+	/**
+	 * Remove a music from the music list.
+	 * @param music - Remove music of the type Music
+	 */
+	public void removeMusicList(Music music) {
+		musicList.remove(music);
+	}
+	public void clearMusicList() {
+		musicList.clear();
+	}
+	/**
+	 * Play the music
+	 */
 	public void play() {
 		if(clip == null) return;
 		stop();
 		clip.setFramePosition(0);
 		clip.start();
 	}
-	
+	/**
+	 * Stop the current music.
+	 */
 	public void stop() {
 		if(clip.isRunning()) clip.stop();
 	}
-	
+	/**
+	 * Close the  current music.
+	 */
 	public void close() {
 		stop();
 		clip.close();
