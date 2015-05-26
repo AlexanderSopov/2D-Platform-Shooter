bb98d4b3d58c409c23261f14332fcaf09c69ad9a
Martin Tran <martin_t_39@hotmail.com>
2015-05-26 07:20:03 +0200

    prepares for merge

--- src/main/java/edu/chl/Game/controller/RefreshTimer.java
+++ src/main/java/edu/chl/Game/controller/RefreshTimer.java
@@ -32,7 +32,7 @@ public class RefreshTimer extends Observable implements Runnable{
 	private Frame frame;
 	
 	//The state of the game
-	public static State state = State.MAP;
+	public static State state = State.GAME;
 	//Array of possible levels
 	public static String[] levels = {"level_1","level_2", "level_3", "level_4", "level_5"};
 	//The selected map/level
