9826903a02ac196d8edfd00ffd0447821de49b70
Rasmus <rasmus.webs@gmail.com>
2015-05-27 09:22:16 +0200

    test

---
+++ src/test/java/edu/chl/Game/model/gameobject/entity/player/TestBullet.java
@@ -0,0 +1,33 @@
+package edu.chl.Game.model.gameobject.entity.player;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.chl.Game.controller.GameHandler;
+import edu.chl.Game.model.gameobject.Id;
+import edu.chl.Game.model.physics.ProjectileDetection;
+import edu.chl.Game.view.Frame;
+
+public class TestBullet {
+	
+	private Bullet testBullet;
+	
+	
+	@Before 
+	public void initBullet(){
+		//   testBullet = 
+	}
+
+
+    @Test
+    public void updateFunctionTest(){
+    	
+    }
+
+
+}
